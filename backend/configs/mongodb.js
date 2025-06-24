2aae0413369e15f8860bfe0c88510684e83ba2f3
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-06-24 14:56:03 +0530

    gggg

--- backend/configs/mongodb.js
+++ backend/configs/mongodb.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
-  mongoose.connection.on('connected', ()=> console.log('Database Connected'))
+const connectDB = async () => {
+  mongoose.connection.on("connected", () => console.log("✅ Database Connected"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/edura`)
-}
-  
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
