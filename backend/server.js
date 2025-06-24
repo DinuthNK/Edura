8e96ce4f92a1e4bdd06ac5b08045869714aee71d
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-06-24 13:57:03 +0530

    rgeg

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config';
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js';
+import clerkWebhookRoutes from './routes/clerkWebhook.js';
 
 // initialize express
 const app = express()
@@ -14,8 +14,7 @@ await connectDB()
 app.use(cors())
 
 //route
-app.get('/',(req,res)=> res.send("API Working"))
-app.post('/clerk', express.json(),clerkWebhooks)
+app.use('/api/clerk', clerkWebhookRoutes);
 //port
 const PORT = process.env.PORT || 5000
 
