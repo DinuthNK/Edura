001d3870854fcb2a8de31208d8531f60947af674
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-06-25 09:01:56 +0530

    fd

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.get('/', (req, res) => res.send("API Working"));
 //port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
-  console.log(`Server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
