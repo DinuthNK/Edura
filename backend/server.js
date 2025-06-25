44aa8fabca343c6731ea0e9efeda17321aad3fcd
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-06-25 10:19:15 +0530

    f

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongodb.js';
-import 'dotenv/config'; // This loads environment variables
+import 'dotenv/config'; 
 
 // Initialize express
 const app = express();
 
 // Connect to MongoDB
-await connectDB(); // Note: top-level await requires Node.js v14+ and "type": "module" in package.json
+await connectDB(); 
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"));
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`);
+})
