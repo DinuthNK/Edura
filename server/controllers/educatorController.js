cf76022ca059df5d28259751c57683ad7baa286a
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-06-25 23:09:21 +0530

    uhy

---
+++ server/controllers/educatorController.js
@@ -0,0 +1,19 @@
+import {clerkClient}from '@clerk/express'
+
+// update roleto educator
+export const updateRoleToEducator = async (req, res)=> {
+    try {
+        const userId = req.auth.userId
+
+        await  clerkClient.users.updateUserMetadata(userId,{
+            publicMetadata:{
+                role: 'educator',
+            }
+        })
+
+        res.json({success: true, message: 'You can publishacourse now'})
+
+    } catch (error) {
+       res.json({success: false, message: error.message}) 
+    }
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
 
 
 //initalize express
@@ -13,12 +15,13 @@ await connectDB()
 
 // Middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"));
 
 app.post('/clerk' , express.json(), clerkWebhooks)
-
+app.use('/api/educator', express.json(), educatorRouter)
 
 //port
 const PORT = process.env.PORT || 5000;
