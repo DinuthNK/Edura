cb8a42721abd618ccb46d4cf63b0d01acb4dfcd1
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-06-25 23:25:21 +0530

    eee

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -11,7 +11,7 @@ export const updateRoleToEducator = async (req, res)=> {
             }
         })
 
-        res.json({success: true, message: 'You can publishacourse now'})
+        res.json({success: true, message: 'You can publish a course now'})
 
     } catch (error) {
        res.json({success: false, message: error.message}) 
