a38e3a8764d8fc3e5b3e8a48445b58590c576616
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-06-26 15:09:32 +0530

    d

--- server/models/Course.js
+++ server/models/Course.js
@@ -29,9 +29,14 @@ const courseSchema =  new mongoose.Schema({
     courseRatings:[
         {  userId:{type: String}, rating:{type: Number, min:1, max: 5}}
     ],
-    educator: { type: Number, ref: 'User', required: true},
+    educator: { type: String, ref: 'User', required: true},
     enrolledStudents:[
      { type: String, ref: 'User'}
     ],
 
 }, {timestamps: true, minimize: false})
+
+
+const Course = mongoose.model('Course', courseSchema)
+
+export default Course;
--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { requireAuth } from '@clerk/express';
-import { updateRoleToEducator } from '../controllers/educatorController.js';
+
+import { addCourse, updateRoleToEducator } from '../controllers/educatorController.js';
+import upload from '../configs/multer.js';
+import { protectEducator } from '../middlewares/AuthMIddleware.js';
 
 const educatorRouter = express.Router();
 
 // Protect this route with Clerk
-educatorRouter.get('/update-role', requireAuth(), updateRoleToEducator);
+educatorRouter.get('/update-role',  updateRoleToEducator);
+educatorRouter.post('/add-course', upload.single('image'),
+ protectEducator, addCourse)
+
+
 
 export default educatorRouter;
