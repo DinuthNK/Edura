3b864ef653ca43b75ae4534ef2cf94293345b7fd
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-07-16 18:54:43 +0530

    ded

--- server/middlewares/AuthMIddleware.js
+++ server/middlewares/AuthMIddleware.js
@@ -1,22 +1,16 @@
-import { clerkClient } from "@clerk/express"
+import { clerkClient } from "@clerk/express";
 
-// Middleware ( Protect Educator Routes )
-export const protectEducator = async (req,res,next) => {
+export const protectEducator = async (req, res, next) => {
+  try {
+    const userId = req.auth.userId;
+    const response = await clerkClient.users.getUser(userId);
 
-    try {
-
-        const userId = req.auth.userId
-        
-        const response = await clerkClient.users.getUser(userId)
-
-        if (response.publicMetadata.role !== 'educator') {
-            return res.json({success:false, message: 'Unauthorized Access'})
-        }
-        
-        next ()
-
-    } catch (error) {
-        res.json({success:false, message: error.message})
+    if (response.publicMetadata.role !== "educator") {
+      return res.status(403).json({ success: false, message: "Unauthorized Access" });
     }
 
-}
+    next();
+  } catch (error) {
+    return res.status(401).json({ success: false, message: error.message });
+  }
+};
