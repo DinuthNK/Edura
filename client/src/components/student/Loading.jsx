d2c58c7c97a126212801d2496ac67eb6f721314b
Dinuth Nadeepa <dinuthnadeepa1302gmail.com>
2025-07-17 00:43:05 +0530

    fswd

--- client/src/components/student/Loading.jsx
+++ client/src/components/student/Loading.jsx
@@ -1,27 +1,26 @@
-import React, { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Loading = () => {
+  const { path } = useParams();
+  const navigate = useNavigate();
 
-  const { path } = useParams()
-  const navigate = useNavigate()
+  useEffect(() => {
+    if (path) {
+      const timer = setTimeout(() => {
+        navigate(`/${path}`);
+      }, 5000);
 
-  useEffect(()=>{
-    if(path){
-      const timer = setTimeout(()=>{
-        navigate(`/${path}`)
-      }, 5000)
-      return ()=> clearTimeout(timer);
+      // Cleanup the timer on component unmount
+      return () => clearTimeout(timer);
     }
-  },[])
+  }, []);
+
   return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <div className='w-16 sm:w-20 aspect-square border-4 border-gray-300 border-t-4
-       border-t-purple-400 rounded-full animate-spin'> 
-
-      </div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-16 sm:w-20 aspect-square border-4 border-gray-300 border-t-4 border-t-blue-400 rounded-full animate-spin"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
